85d859150cb24e8332c6416b81b4285c8150e34d
Jawen Voon <weinchester@gmail.com>
2019-01-06 22:53:39 +0800

    fix import on app-routing module

--- starwarsguide/src/app/app-routing.module.ts
+++ starwarsguide/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 import {CategoryComponent} from './category/category.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DetailComponent } from './detail/detail.component';
